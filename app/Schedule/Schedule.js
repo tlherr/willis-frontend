da2d53b886eb2324dc1c05c7ae9fe25e17a60b49
Thomas Herr <thom1810@gmail.com>
2017-02-10 22:06:39 -0500

    Schedule page now working

---
+++ app/Schedule/Schedule.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('Willis.Schedule', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/schedule', {
+    templateUrl: 'Schedule/Schedule.html',
+    controller: 'ScheduleCtrl'
+  });
+}])
+
+.controller('ScheduleCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+    $http.get('http://data.willischurch.ca/api/schedule')
+        .then(function(res){
+            $scope.schedules = res.data;
+            console.log(res.data);
+        });
+}]);
